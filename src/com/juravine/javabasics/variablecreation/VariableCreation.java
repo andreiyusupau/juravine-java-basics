c9a201f9f0c89735e4ceb82393e1ea3a86dffbdb
Andrei Yusupau <andrei.yusupau.vd@yandex.ru>
2023-06-03 14:46:46 +0300

    Add variable creation source code.

---
+++ src/com/juravine/javabasics/variablecreation/VariableCreation.java
@@ -0,0 +1,25 @@
+package com.juravine.javabasics.variablecreation;
+
+public class VariableCreation {
+
+    public static void main(String... args) {
+        //variable declaration
+        int a;
+
+        //multiple variables declaration
+        int b,c,d;
+
+        //separate variable declaration and initialization
+        int x;
+        x = 5;
+        System.out.println(x);
+
+        //combined variable declaration and initialization
+        int y = 5;
+        System.out.println(y);
+
+        //multiple variable declaration and initialization
+        int r, s, t = 5;
+        System.out.println(t);
+    }
+}
